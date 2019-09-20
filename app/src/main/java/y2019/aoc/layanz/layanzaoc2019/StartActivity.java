a0997e3fa3318ca7fe9e601b539629175094cf79
?SlimShadyE <ramiar2410@gmail.com>
2019-09-20 09:15:03 +0300

    intent

---
+++ app/src/main/java/y2019/aoc/layanz/layanzaoc2019/StartActivity.java
@@ -0,0 +1,31 @@
+package y2019.aoc.layanz.layanzaoc2019;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class StartActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button buttonStart;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+
+
+        TextView start;
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+    }
+}
+
